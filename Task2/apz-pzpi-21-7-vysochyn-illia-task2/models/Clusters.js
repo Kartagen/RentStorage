af893c76fadbc99960834ef2ad6e35649e016c10
EquesAuratus <illia.vysochyn@nure.ua>
2024-08-24 20:38:40 +0300

    coping project

---
+++ Task2/apz-pzpi-21-7-vysochyn-illia-task2/models/Clusters.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const clustersSchema = new mongoose.Schema({
+    name: {type: String, required: true, unique: true},
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+    city: { type: String, required: true },
+    type: {type: String, required: true},
+    workTime: {
+        from: {
+            type: String,
+            validate: {
+                validator: function (v) {
+                    if (!v) {
+                        return true;
+                    }
+                    return /([01]?[0-9]|2[0-3]):[0-5][0-9]/.test(v);
+                },
+                message: (props) => `${props.value} is not valid time!`,
+            },
+            required: true
+        },
+        to: {
+            type: String,
+            validate: {
+                validator: function (v) {
+                    if (!v) {
+                        return true;
+                    }
+                    return /([01]?[0-9]|2[0-3]):[0-5][0-9]/.test(v);
+                },
+                message: (props) => `${props.value} is not valid time!`,
+            },
+            required: true
+        },
+    },
+}).index({ location: '2dsphere' });
+const Clusters = mongoose.model('Clusters', clustersSchema);
+module.exports = Clusters;
